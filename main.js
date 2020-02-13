f9a03a4316a6d37b9de585786a8b242f87e5a392
Jianan Ding <jianan.ding1@ucalgary.ca>
2020-02-13 06:42:59 -0700

    Update javascript

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 const expression = document.querySelector("#expression");
 const result = document.querySelector("#result");
 
+
 const left_bracket_button = document.querySelector("#left-bracket");
 left_bracket_button.addEventListener("click", () => {
-    // no rule for this
-    expression.textContent = "(";
+    // if at the beginning, zero is clicked, then click this button will allow and show '0('
+    if (expression.textContent === "0") {
+        expression.textContent = "(";
+    } else {
+        expression.textContent += "(";
+    }
 });
 
 const right_bracket_button = document.querySelector("#right-bracket");
@@ -19,35 +24,62 @@ right_bracket_button.addEventListener("click", () => {
 const clear_button = document.querySelector("#clear");
 clear_button.addEventListener("click", () => {
     // clear all expression, no rule applied
-    expression.textContent = "";
+    expression.textContent = "0";
 });
 
 const backspace_button = document.querySelector("#backspace");
 backspace_button.addEventListener("click", () => {
     // delete the last charactor in expression, no rule applied
-    expression.textContent = expression.textContent.slice(0, -1);
+    if (expression.textContent.slice(0, -1).length === 0) {
+        expression.textContent = "0";
+    } else {
+        expression.textContent = expression.textContent.slice(0, -1);
+    }
 });
 
 const divide_button = document.querySelector("#divide");
 divide_button.addEventListener("click", () => {
-    expression.textContent += "/";
+    if (expression.textContent.slice(-1) === "+" || expression.textContent.slice(-1) === "-" || expression.textContent.slice(-1) === "*" || expression.textContent.slice(-1) === "/") {
+        if (expression.textContent.charAt(expression.textContent.length-2) !== "(") {
+            expression.textContent = expression.textContent.slice(0, -1) + "/";
+        }
+    } else if (expression.textContent.slice(-1) !== "(") {
+        expression.textContent += "/";
+    }
 });
 
 const multiply_button = document.querySelector("#multiply");
 multiply_button.addEventListener("click", () => {
-    expression.textContent += "*";
-});
+    if (expression.textContent.slice(-1) === "+" || expression.textContent.slice(-1) === "-" || expression.textContent.slice(-1) === "*" || expression.textContent.slice(-1) === "/") {
+        if (expression.textContent.charAt(expression.textContent.length-2) !== "(") {
+            expression.textContent = expression.textContent.slice(0, -1) + "*";
+        }
+    } else if (expression.textContent.slice(-1) !== "(") {
+        expression.textContent += "*";
+    }});
 
 const minus_button = document.querySelector("#minus");
 minus_button.addEventListener("click", () => {
-    expression.textContent += "-";
+    if (expression.textContent.slice(-1) === "+" || expression.textContent.slice(-1) === "-" || expression.textContent.slice(-1) === "*" || expression.textContent.slice(-1) === "/") {
+        if (expression.textContent.charAt(expression.textContent.length-2) !== "(") {
+            expression.textContent = expression.textContent.slice(0, -1) + "*";
+        }
+    } else {
+        expression.textContent += "-";
+    }
+    // case that initial expression is not pressed value "0", user press button replace "0" with "-"
 });
 
 const dot_button = document.querySelector("#dot");
 dot_button.addEventListener("click", () => {
     // if the last charactor is '.', then this not allow user to add another dot
     if (expression.textContent.slice(-1) !== ".") {
-        expression.textContent += ".";
+        // if the last charactor is "(", add a "*."
+        if (expression.textContent.slice(-1) === ")") {
+            expression.textContent += "*.";
+        } else {
+            expression.textContent += ".";
+        }
     }
 });
 
@@ -58,7 +90,12 @@ equal_button.addEventListener("click", () => {
 
 const plus_button = document.querySelector("#plus");
 plus_button.addEventListener("click", () => {
-    expression.textContent += "+";
+    if (expression.textContent.slice(-1) === "+" || expression.textContent.slice(-1) === "-" || expression.textContent.slice(-1) === "*" || expression.textContent.slice(-1) === "/") {
+        expression.textContent = expression.textContent.slice(0, -1) + "+";
+    }
+    else if (expression.textContent.slice(-1) !== "(") {
+        expression.textContent += "+";
+    }
 });
 
 const num_buttons = document.querySelectorAll("#num");
